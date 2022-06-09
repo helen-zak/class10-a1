b06700c2965a33cd8e1ec7fceb2149a93f9fdd64
helen-zak <106342919+helen-zak@users.noreply.github.com>
2022-06-09 18:25:24 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var weights=[35,38,40,45]
+function setup() 
+{
+  createCanvas(400,400);
+  var sum=weights[0]+weights[1]+weights[2]+weights[3]
+  console.log(sum)
+  var avg=sum/weights.length
+  console.log(avg)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
